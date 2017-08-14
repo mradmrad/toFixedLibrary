0d4aea3963246cd7b04db58077fdfcfd5557b68c
mradmrad <haithemmrad22@gmail.com>
2017-08-14 14:18:12 +0100

    first commit

---
+++ toFixed.js
@@ -0,0 +1,16 @@
+/**
+ * Created by HAITHEM on 14/08/2017.
+ */
+
+var numbers = {
+    init: function(){
+
+        $(".tofixed").each(function () {
+            // alert('here');
+            var my_number = ($(this).val() == '') ? 0 : parseFloat($(this).val());
+            $(this).val(my_number.toFixed(3));
+        });
+    }
+};
+
+ 
